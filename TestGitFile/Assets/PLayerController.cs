3e8541d617f86557e75515fd78943762f412c109
stassak <37385622+stassak@users.noreply.github.com>
2025-01-12 19:29:27 +0200

    Adding Player Scripts
    Adding Player Scripts:
    Moving and Damage.

---
+++ TestGitFile/Assets/PLayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public Slider healthSlider; // Reference to the health slider
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth; // Initialize player's health
+
+        // Ensure the slider reflects the player's starting health
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+        }
+        else
+        {
+            Debug.LogError("Health Slider is not assigned!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Ensure health doesn't go below 0
+
+        Debug.Log("Player Health: " + currentHealth);
+
+        // Update the slider's value
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player has died!");
+        // Add death logic here
+    }
+}
---
+++ TestGitFile/Assets/PlayerMove.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+        transform.Translate(movement, Space.World);
+    }
+}
