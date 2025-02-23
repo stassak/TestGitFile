ab7abfad9242ac997594254bddb1068e2fa87469
stassak <37385622+stassak@users.noreply.github.com>
2025-02-23 20:35:20 +0200

    Change the Enemy

--- TestGitFile/Assets/PlayerUnity.cs
+++ TestGitFile/Assets/PlayerUnity.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class PlayerUnity : MonoBehaviour
 {
     private Rigidbody playerRB;
+    public float jumpForce;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
-        
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRB.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
