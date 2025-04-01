d286a1691fb6a6edb7d7001c6982e386afd158a2
stassak <37385622+stassak@users.noreply.github.com>
2025-04-02 01:16:28 +0300

    Adding script Manag.Sys

--- TestGitFile/Assets/Scripts1/GameManager.cs
+++ TestGitFile/Assets/Scripts1/GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
